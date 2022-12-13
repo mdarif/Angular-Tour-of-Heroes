35a89280dbf61a5d9a47565f4e04ec6b358abe6f
Mohammad Arif <arif_iq@yahoo.co.in>
2022-12-13 21:36:20 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
+/**
+ * NgModules configure the injector and the compiler and help
+ * organize related things together.
+ *
+ * Every Angular app has at least one NgModule class, the root module
+ * that you bootstrap to launch the application. By convention, it is
+ * usually called 'AppModule'.
+ */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeroesComponent],
+  /**
+   * Add 'FormsModule' to the 'imports' array in @NgModule.
+   * The 'imports' array contains the list of external modules that the application needs.
+   */
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+/**
+ * @Component is a decorator function that specifies the Angular
+ * metadata for the component.
+ *
+ * The CLI generated three metadata properties:
+ *
+ * selector— the component's CSS element selector
+ * templateUrl— the location of the component's template file.
+ * styleUrls— the location of the component's private CSS styles.
+ *
+ * The CLI generated the component class and exported it so you can
+ * import it elsewhere ... like in the AppModule.
+ *
+ * The component class controls the appearance and behavior of a view
+ * through its template.
+ */
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent {
+  /**
+   * Refactor the component's hero property to be of type 'Hero'.
+   * Initialize it with an id of 1 and the name 'Windstorm'.
+   */
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
+}
